27444eab17233fd745337da39e705c50fa7ced05
Joshua <jgb16@my.fsu.edu>
2022-01-19 22:33:24 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+let firstCard = 10
+let secondCard = 11
+let sum = firstCard + secondCard
+
+let hasBlackJack = false 
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl= document.getElementById("cards-el")
+
+function startGame() {
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent ="Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    }
+    else if (sum === 21) {
+        message = "Woohoo! You have Blackjack!"
+        hasBlackJack = true 
+    }
+    else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    
+    messageEl.textContent = message
+}
+
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+    let card = 4
+    sum += card
+    renderGame() 
+
+}
