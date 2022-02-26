2fb610b64185fc4b487439dd4ea829fbe3b8f79e
Joshua <jgb16@my.fsu.edu>
2022-02-25 19:03:50 -0500

    added rules and mobile layout

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 
-let player = {
-    name: "test", 
-    chips: 2000
-}
+
 let sum = 0
 let cards = [] 
 let hasBlackJack = false 
@@ -11,9 +8,9 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl= document.getElementById("cards-el")
-let playerEl = document.getElementById("player-el")
+let rulesEl = document.getElementById("rules-el")
 
-playerEl.textContent = player.name + ": $" + player.chips 
+rulesEl.textContent = "Simplified BlackJack rules: Your draw cannot exceed 21 or else you lose the game. To win you must draw exactly 21"
 function getRandomCard() { 
     let randCard = Math.floor(Math.random() * 13) + 1
     if (randCard === 1) {
@@ -37,8 +34,9 @@ function startGame() {
 function renderGame() {
     
     cardsEl.textContent ="Cards: "
+    cardsEl.style.whiteSpace = "pre"
     for (let i = 0; i < cards.length; i++){
-        cardsEl.textContent += cards[i] + " " 
+        cardsEl.textContent += cards[i] + "   " 
     }
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
@@ -63,7 +61,9 @@ function newCard() {
     cards.push(card)
     renderGame() 
     }
-    else {
-        renderGame()
+    else if (hasBlackJack === true ){
+        hasBlackJack = false
+        startGame()
     }
+
 }
